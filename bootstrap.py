75b8e3ef900161edc37470dcf85d2a0345dbb296
Neal <neal@pricepointmoves.com>
2021-06-28 15:29:11 -0500

    fix up bootstrap

--- bootstrap.py
+++ bootstrap.py
@@ -1,50 +1,69 @@
-from __future__ import print_function
+import os
 from builtins import input
-import subprocess
-import getpass
+from pathlib import Path
 
-def exit_if_no(to_install, op):
-  print('To install dev environment you need to ' + op)
-  print(to_install)
-  do_install = input('do you want to continue (y | n)')
-  if (do_install.lower() != 'y'):
-    exit(1)
+def multi_line_input(prompt):
+      print("Enter/Paste " + prompt + ". Enter Ctrl-D to save it.")
+      contents = []
+      while True:
+            try:
+              line = input()
+            except EOFError:
+              break
+            contents.append(line)
+      return '\n'.join(contents)
 
-apt_packages_to_install = [
-  'python-pip',
-  'python3-venv',
-  'python3-pip',
-]
+# check if the user want so update their ssh keys
+ssh_directory=os.path.join(str(Path.home()),'.ssh');
+Path(ssh_directory).mkdir(parents=True, exist_ok=True)
+update_ssh = input('Do you want to update existing ssh keys (y | n)? ')
+if update_ssh.lower() == 'y':
+      public_key = multi_line_input('public ssh key')
+      private_key = input('private ssh key ')
+      with open(os.path.join(ssh_directory, 'id_rsa.pub'), 'wb') as id_rsa_pub:
+            id_rsa_pub.write(public_key.encode());
+      with open(os.path.join(ssh_directory, 'id_rsa'), 'wb') as id_rsa:
+            id_rsa.write(private_key.encode());
+      os.chmod(os.path.join(ssh_directory, 'id_rsa'), 0o600)
+      os.chmod(os.path.join(ssh_directory, 'id_rsa.pub'), 0o600)
+else:
+      # to do here generate ssh keys and do the github login dance to
+      # to set them up
+      print('It is recommended that you have ssh keys')
 
-pip_packages_to_install = [
-  'requests'
-]
+# create .ppbashrc and add it to .bashrc
+ppbashrc_directory=os.path.join(str(Path.home()),'.ppbashrc')
+Path(ppbashrc_directory).mkdir(parents=True, exist_ok=True)
+aws_creds_filename=os.path.join(ppbashrc_directory, '.aws_creds')
+ppbashrc_contents="""
+source {0}
+""".format(
+      aws_creds_filename
+)
+ppbashrc_filename = os.path.join(ppbashrc_directory, '.bashrc')
+with open(ppbashrc_filename, 'w') as ppbashrc:
+      ppbashrc.write(ppbashrc_contents);
 
-exit_if_no('\n'.join(apt_packages_to_install + pip_packages_to_install), 'install')
+bashrc_content_line='source ' + ppbashrc_filename
+found = False
+bashrc_filename=os.path.join(str(Path.home()),'.bashrc')
+with open(bashrc_filename, 'r') as bashrc:
+      for line in bashrc:
+            if line == bashrc_content_line:
+                  found = True
+if found == False:
+      with open(bashrc_filename, 'a') as bashrc:
+            bashrc.write(bashrc_content_line)
 
-subprocess.check_call('sudo apt-get update', shell=True)
-subprocess.check_call('sudo apt-get install -y ' + ' '.join(apt_packages_to_install), shell=True)
-subprocess.check_call('sudo pip install ' + ' '.join(pip_packages_to_install), shell=True)
-
-import requests
-
-username = input('GitHub Username:')
-password = getpass.getpass()
-
-requirements = requests.get(
-  'https://api.github.com/repos/Price-Point/python_misc/contents/requirements.txt',
-  headers= { 'Accept': 'application/vnd.github.v3.raw' },
-  auth=(username,password))
-
-exit_if_no(requirements.text, 'install')
-
-subprocess.check_call('sudo -H pip install ' + ' '.join(pip_packages_to_install), shell=True)
-
-dev_env_setup = requests.get(
-  'https://api.github.com/repos/Price-Point/python_misc/contents/dev_env_setup.py',
-  headers= { 'Accept': 'application/vnd.github.v3.raw' },
-  auth=(username,password))
-
-exit_if_no(dev_env_setup.text, 'execute')
-
-exec(dev_env_setup.text)
+# get aws-cli login info
+update_aws_creds  = input('Do you want to update your aws creds (y | n)? ')
+if update_aws_creds.lower() == 'y':
+      aws_access_key=input('aws access key ')
+      aws_secret=input('aws secret ')
+      aws_creds_contents="""export AWS_ACCESS_KEY_ID={0}
+export AWS_SECRET_ACCESS_KEY={1}
+export AWS_DEFAULT_REGION=us-east-1
+export AWS_DEFAULT_OUTPUT=json
+""".format(aws_access_key,aws_secret)
+      with open(aws_creds_filename, 'w') as bashrc:
+            bashrc.write(aws_creds_contents)
---
+++ bootstrap.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+if [ $EUID = 0 ]
+  then echo "Please run as non root user"
+  exit
+fi
+
+echo "add git and ansible repositories"
+set ADD_REPOSITORIES="sudo add-apt-repository ppa:git-core/ppa ppa:ansible/ansible"
+echo $ADD_REPOSITORIES
+$ADD_REPOSITORIES
+echo "install common dependencies"
+set UPDATE_APT="sudo apt update"
+echo $UPDATE_APT
+$UPDATE_APT
+set INSTALL_DEPS="
+sudo apt-get install -y build-essential \
+curl \
+software-properties-common \
+python3 \
+git \
+ansible \
+snapd \
+snapd-xdg-open \
+python3-pip \
+python3-venv \
+apt-transport-https \
+ca-certificates \
+curl \
+gnupg \
+lsb-release
+"
+echo $INSTALL_DEPS
+$INSTALL_DEPS
+INSTALL_PYTHON_DEPS="sudo pip3 install boto3 \
+awscli \
+"
+echo $INSTALL_PYTHON_DEPS
+$INSTALL_PYTHON_DEPS
+INSTALL_PYTHON_DEPS="sudo pip3 install boto3 \
+awscli \
+"
+echo $INSTALL_PYTHON_DEPS
+$INSTALL_PYTHON_DEPS
+echo "install chrome"
+GET_CHROME="wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+echo $GET_CHROME
+$GET_CHROME
+INSTALL_CHROME="sudo apt install -y ./google-chrome-stable_current_amd64.deb"
+echo $INSTALL_CHROME
+$INSTALL_CHROME
+REMOVE_CHROME_INSTALLER="rm ./google-chrome-stable_current_amd64.deb"
+echo $REMOVE_CHROME_INSTALLER
+$REMOVE_CHROME_INSTALLER
+echo "install postman"
+INSTALL_POSTMAN="sudo snap install postman"
+echo $INSTALL_POSTMAN
+$INSTALL_POSTMAN
+UPDATE_POSTMAN="sudo snap refresh postman"
+echo $UPDATE_POSTMAN
+$UPDATE_POSTMAN
+echo "install node"
+INSTALL_NODE="sudo snap install node --classic --channel=14"
+echo $INSTALL_NODE
+$INSTALL_NODE
+UPDATE_NODE="sudo snap refresh node --classic --channel=14"
+echo $UPDATE_NODE
+$UPDATE_NODE
+echo "install vscode"
+INSTALL_CODE="sudo snap install --classic code"
+echo $INSTALL_CODE
+$INSTALL_CODE
+UPDATE_CODE="sudo snap refresh --classic code"
+echo $UPDATE_CODE
+$UPDATE_CODE
+echo "install docker"
+DOCKER_INSTALL_SRCIPT="curl -fsSL https://get.docker.com -o get-docker.sh"
+echo $DOCKER_INSTALL_SRCIPT
+$DOCKER_INSTALL_SRCIPT
+INSTALL_DOCKER="sudo sh get-docker.sh"
+echo $INSTALL_DOCKER
+$INSTALL_DOCKER
+RM_DOCKER="rm get-docker.sh"
+echo $RM_DOCKER
+$RM_DOCKER
+CREATE_DOCKER_GROUP="sudo groupadd docker"
+echo $CREATE_DOCKER_GROUP
+$CREATE_DOCKER_GROUP
+ADD_USER_TO_DOCKER="sudo usermod -aG docker $USER"
+echo $ADD_USER_TO_DOCKER
+$ADD_USER_TO_DOCKER
+# wget -qO- https://api.github.com/repos/Price-Point/DevelopmentBoostrap/contents/bootstrap.py --header 'Accept: application/vnd.github.v3.raw' | sudo python3 -
+python3 bootstrap.py
+
+
